7715d9dc899bad165513dea2a04bb81651eea03e
Conrad Trost <ct.conradtrost@gmail.com>
2023-03-09 17:13:35 -0600

    feat(recipes): add feature search lib

--- apps/recipes/src/app/remote-entry/entry.component.ts
+++ apps/recipes/src/app/remote-entry/entry.component.ts
@@ -1,11 +1,12 @@
 import { Component } from '@angular/core';
 import { CommonModule } from '@angular/common';
-import { NxWelcomeComponent } from './nx-welcome.component';
+
+import { SearchComponent } from '@eaa/recipes/feature/search';
 
 @Component({
   standalone: true,
-  imports: [CommonModule, NxWelcomeComponent],
-  selector: 'enterprise-angular-architecture-recipes-entry',
-  template: `<enterprise-angular-architecture-nx-welcome></enterprise-angular-architecture-nx-welcome>`,
+  imports: [CommonModule, SearchComponent],
+  selector: 'eaa-recipes-entry',
+  template: `<eaa-recipes-search></eaa-recipes-search>`,
 })
 export class RemoteEntryComponent {}
---
+++ libs/recipes/feature/search/README.md
@@ -0,0 +1,7 @@
+# recipes-feature-search
+
+This library was generated with [Nx](https://nx.dev).
+
+## Running unit tests
+
+Run `nx test recipes-feature-search` to execute the unit tests.
